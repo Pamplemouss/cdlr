96af0beb4362a9742d04eb856a1ac0f875a76af1
Pamplemouss <john.dufaye@gmail.com>
2025-02-26 04:50:00 +0100

    [V3] Roles

--- src/app/components/Roles.tsx
+++ src/app/components/Roles.tsx
@@ -1,43 +1,106 @@
 import React from "react";
 import Header from "./Header";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Roles = () => {
-  return (
-    <div className="overflow-hidden py-8 my-10">
-      <Header
-        title="Découvrez votre rôle !"
-        content="Soyez mousquetaire et ramenez le collier à la Reine, ou soyez espion et sabotez la mission sans vous faire repérer !"
-      />
-      <div className="my-10 flex w-full gap-3">
-        <div className="w-1/2 flex justify-end">
-          <div className="w-[calc(100%-75px)] relative">
-            <img src="/roles/musk3.jpg" className="w-full z-30 relative" />
-            <img
-              src="/roles/musk1.jpg"
-              className="w-full absolute top-0 -left-4 -rotate-6 z-20"
+    return (
+        <div className="overflow-hidden py-8 my-10">
+            <Header
+                title="Découvrez votre rôle !"
+                content="Soyez mousquetaire et ramenez le collier à la Reine, ou soyez espion et sabotez la mission sans vous faire repérer !"
             />
-            <img
-              src="/roles/musk2.jpg"
-              className="w-full absolute top-0 -left-8 -rotate-12 z-10"
-            />
-          </div>
+            <div className="my-10 flex w-full gap-5 relative">
+                <div className="w-1/2 flex justify-end">
+                    <div className="w-[calc(100%-75px)] relative -rotate-6">
+                        <motion.img
+                            src="/roles/musk1.jpg"
+                            initial={{ opacity: 0, x: -20 }}
+                            animate={{ y: [0, -5, 0] }}
+                            whileInView={{ opacity: 1, x: 0, transition: { duration: 0.4, delay: 0.2 } }}
+                            viewport={{ once: true }}
+                            transition={{
+                                duration: 5,
+                                repeat: Infinity,
+                                ease: "easeInOut",
+                            }}
+                            className="w-full z-30 relative"
+                        />
+
+                        <motion.img
+                            src="/roles/musk2.jpg"
+                            initial={{ opacity: 0, x: -20 }}
+                            animate={{ y: [0, -5, 0], rotate: [-6, -6, -6] }}
+                            whileInView={{ opacity: 1, x: 0, transition: { duration: 0.4, delay: 0.4 } }}
+                            viewport={{ once: true }}
+                            transition={{
+                                duration: 5,
+                                repeat: Infinity,
+                                ease: "easeInOut",
+                                delay: 1,
+                            }}
+                            className="w-full absolute top-0 -left-4 z-20"
+                        />
+                        <motion.img
+                            src="/roles/musk3.jpg"
+                            initial={{ opacity: 0, x: -20 }}
+                            animate={{ y: [0, -5, 0], rotate: [-12, -12, -12] }}
+                            whileInView={{ opacity: 1, x: 0, transition: { duration: 0.4, delay: 0.6 } }}
+                            viewport={{ once: true }}
+                            transition={{
+                                duration: 5,
+                                repeat: Infinity,
+                                ease: "easeInOut",
+                                delay: 2,
+                            }}
+                            className="w-full absolute top-0 -left-8 z-10"
+                        />
+                    </div>
+                </div>
+                <div className="w-1/2 flex justify-begin">
+                    <div className="w-[calc(100%-75px)] relative rotate-6">
+                        <motion.img
+                            src="/roles/spy1.jpg"
+                            initial={{ opacity: 0, x: 20 }}
+                            animate={{ y: [0, -5, 0] }}
+                            whileInView={{ opacity: 1, x: 0, transition: { duration: 0.4, delay: 0.2 } }}
+                            viewport={{ once: true }}
+                            transition={{
+                                duration: 5,
+                                repeat: Infinity,
+                                ease: "easeInOut",
+                            }}
+                            className="w-full z-30 relative"
+                        />
+                        <motion.img
+                            src="/roles/spy2.jpg"
+                            initial={{ opacity: 0, x: 20 }}
+                            animate={{ y: [0, -5, 0], rotate: [6, 6, 6] }}
+                            whileInView={{ opacity: 1, x: 0, transition: { duration: 0.4, delay: 0.4 } }}
+                            viewport={{ once: true }}
+                            transition={{
+                                duration: 5,
+                                repeat: Infinity,
+                                ease: "easeInOut",
+                                delay: 1,
+                            }}
+                            className="w-full absolute top-0 left-4 rotate-6 z-20"
+                        />
+                    </div>
+                </div>
+                <motion.img
+                initial={{ scale: 2.5, opacity: 0, x: "-50%", y: "-50%" }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ duration: 0.3, delay: 1 }}
+                    src="/roles/VS.png"
+                    width={80}
+                    height={80}
+                    alt="VS"
+                    className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50"
+                />
+            </div>
         </div>
-        <div className="w-1/2 flex justify-begin">
-          <div className="w-[calc(100%-75px)] relative">
-            <img src="/roles/spy3.jpg" className="w-full z-30 relative" />
-            <img
-              src="/roles/spy1.jpg"
-              className="w-full absolute top-0 left-4 rotate-6 z-20"
-            />
-            <img
-              src="/roles/spy2.jpg"
-              className="w-full absolute top-0 left-8 rotate-12 z-10"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Roles;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,66 +10,73 @@ import Tresors from "./components/Tresors";
 import Collier from "./components/Collier";
 
 export default function Home() {
-  function up() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }
-  
-  return (
-    <div className="w-full h-full relative">
-      <div className="w-screen h-screen fixed z-[-1] grayscale blur-[1px]">
-        <img
-          src="/background.png"
-          className="w-full h-full object-cover"
-          alt="bg"
-        />
-      </div>
-      <div className=" w-screen h-screen fixed z-[-1] bg-slate-900 opacity-[95%]"></div>
+    function up() {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    }
 
-      <div className="py-8">
-        <h1 className="text-white text-center font-old-london text-4xl relative">
-          Le Collier de la Reine
-        </h1>
-      </div>
+    return (
+        <div className="w-full h-full relative">
+            <div className="w-screen h-screen fixed z-[-1] grayscale blur-[1px]">
+                <img
+                    src="/background.png"
+                    className="w-full h-full object-cover"
+                    alt="bg"
+                />
+            </div>
+            <div className=" w-screen h-screen fixed z-[-1] bg-slate-900 opacity-[95%]"></div>
 
-      <div className="text-xs bg-slate-600 mx-4 mb-10 p-3 rounded-lg shadow shadow-black/50 text-white">
-        Le Collier de la Reine est un jeu de société d’aventure à rôles cachés.
-        Vous incarnez un groupe de mousquetaires ayant pour mission de rapporter
-        le collier de la Reine jusqu’à la cour du Roi de France.
-        <br />
-        <br /> Malheureusement pour vous, des espions envoyés par le Cardinal de
-        Richelieu ont infiltré votre groupe ! Ces derniers doivent empêcher
-        discrètement les mousquetaires de rentrer à temps, ou réussir à
-        subtiliser le collier au bon moment. <br />
-        <br />
-        Soyez maître de votre histoire, choisissez le gardien du collier après
-        chaque aventure, et méfiez vous des apparences ...
-      </div>
+            <div className="py-8">
+                <h1 className="text-white text-center font-old-london text-4xl relative">
+                    Le Collier de la Reine
+                </h1>
+            </div>
 
-      <div className="flex flex-col w-full justify-center items-center gap-5">
-        <ContactInfo
-          color="bg-sky-400"
-          icon="fa-regular fa-envelope"
-          text="jeremyetjohn@gmail.com"
-        />
-        <ContactInfo
-          color="bg-sky-400"
-          icon="fa-brands fa-instagram"
-          text="Instagram"
-          link="https://www.instagram.com/jeremyetjohn/"
-        />
-      </div>
+            <div className="text-xs bg-slate-600 mx-4 mb-10 p-3 rounded-lg shadow shadow-black/50 text-white">
+                Le Collier de la Reine est un jeu à rôles cachés où
+                mousquetaires et espions se livrent une bataille d’influence.
+                Les mousquetaires tentent d’assurer leur mission en rassemblant
+                des ressources, tandis que les espions sabotent discrètement
+                tout en semant la discorde.
+                <br />
+                <br />
+                Chacun doit bluffer, accuser et convaincre pour survivre. Les
+                alliances se font et se défont au fil des tours, rendant chaque
+                décision cruciale.
+                <br />
+                <br />À la fin du voyage, l’heure des révélations sonne : les
+                joueurs votent pour éliminer les suspects un par un, jusqu’à ce
+                qu’un camp l’emporte. Serez-vous un fin stratège ou une victime
+                des complots ?
+            </div>
 
-      <Roles />
-      <Aventures />
-      <Ressources />
-      <Tresors />
-      <Collier />
+            <div className="flex flex-col w-full justify-center items-center gap-5">
+                <ContactInfo
+                    color="bg-sky-400"
+                    icon="fa-regular fa-envelope"
+                    text="jeremyetjohn@gmail.com"
+                />
+                <ContactInfo
+                    color="bg-sky-400"
+                    icon="fa-brands fa-instagram"
+                    text="Instagram"
+                    link="https://www.instagram.com/jeremyetjohn/"
+                />
+            </div>
 
-      <div className="flex justify-center w-full mb-10">
-        <div onClick={() => up()} className="w-16 aspect-square bg-slate-700 rounded-full cursoir-pointer flex justify-center items-center shadow shadow-black/50">
-          <i className="fa-solid fa-chevron-up text-lg text-white"></i>
+            <Roles />
+            {/**<Aventures />**/}
+            {/**<Ressources />**/}
+            {/**<Tresors />**/}
+            {/**<Collier />**/}
+
+            <div className="flex justify-center w-full mb-10">
+                <div
+                    onClick={() => up()}
+                    className="w-16 aspect-square bg-slate-700 rounded-full cursoir-pointer flex justify-center items-center shadow shadow-black/50"
+                >
+                    <i className="fa-solid fa-chevron-up text-lg text-white"></i>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
